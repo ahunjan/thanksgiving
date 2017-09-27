857fe12bad94aedea16416a7d2e2dd830a9b8f1f
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-09-27 14:09:05 -0700

    Break something

--- src/main/java/com/starbucks/CaesarCipher.java
+++ src/main/java/com/starbucks/CaesarCipher.java
@@ -18,25 +18,10 @@ public class CaesarCipher {
 	    		.collect(Collectors.joining());
 	}
 
-//	public static String encodeBeforeInline(String s, int shift) {
-//		if (s == null) throw
-//     		new NullPointerException("String cannot be null");
-//		if (s.isEmpty()) return "";
-//	    IntStream chars = s.chars();
-//		Stream<Integer> boxed = chars.boxed();
-//		Stream<String> mapped = boxed.map(new Function<Integer, String>() {
-//			@Override
-//			public String apply(Integer i) {
-//				return "" + processChar((char) i.intValue(), shift);
-//			}
-//		});
-//		String collected = mapped.collect(Collectors.joining());
-//		return collected;
-//	}
 	
 	protected static char processChar(char c, int shift) {
 		if (!Character.isAlphabetic(c)) return c;
-		char preferredA = Character.isUpperCase(c) ? 'A' : 'a';
+		char preferredA = Character.isUpperCase(c) ? 'Z' : 'a';
 		return (char) ((c + (shift % 26) + 26 - preferredA) % 26 + preferredA);
 	}
 
